f6e5ae4cefacb4268b9da666a1419094dfff65b7
Gengyuan Huang <gengyuan@yunnan.localdomain>
2021-01-13 20:06:31 -0700

    final

--- cmput404_lab1_pyscript.py
+++ cmput404_lab1_pyscript.py
@@ -6,13 +6,13 @@ import requests as rqs
 
 def main():
     rqs_version = rqs.__version__
-    rqs_google_response = rqs.get("https://www.google.com")
-    rqs_google_statuscode = rqs_google_response.status_code
-    rqs_google_text = rqs_google_response.text
+    rqs_scriptfile_response = rqs.get("https://github.com/gengyuanhuang/cmput404_lab1/raw/main/cmput404_lab1_pyscript.py")
+    rqs_scriptfile_statuscode = rqs_scriptfile_response.status_code
+    rqs_scriptfile_text = rqs_scriptfile_response.text
 
-    print("The requests library version is: \n" + rqs_version)
-    print("The google home page status code is: \n" + str(rqs_google_statuscode))
-    print("The google home page content is: \n" + rqs_google_text)
+    print("The requests library version is: \n" + rqs_version + "\n")
+    print("The script file raw link status code is: \n" + str(rqs_scriptfile_statuscode) + "\n")
+    print("The script file content is: \n" + rqs_scriptfile_text + "\n")
 
 
 main()
